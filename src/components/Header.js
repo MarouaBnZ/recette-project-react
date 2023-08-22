cb551826f55d6acafbb705e0aea8522ab12da4f8
Maroua Bouanz <maroua.bouanz@gmail.com>
2023-08-22 19:53:24 +0100

    Mise en place de header et de footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
-import React from "react";
-
+import styles from "./Header.module.scss";
+import recette from "../assets/images/recetteTr.png";
+{
+  /* <i class="fa-solid fa-bars"></i> */
+}
 function Header() {
-  return <div className="b1">Header</div>;
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <i class="fa-solid fa-bars mr-15"></i>
+      <div className="flex-fill">
+        <img src={recette} alt="logo-recette" />
+      </div>
+      <ul>
+        <button className="mr-5 btn btn-reverse-primary">
+          <i class="fa-solid fa-basket-shopping mr-5"></i>
+          <span>Panier</span>
+        </button>
+        <button className="btn btn-primary">Connexion</button>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
