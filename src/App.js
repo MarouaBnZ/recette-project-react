244859ef5bb61ca476c8ad5297b5ed0679781869
Maroua Bouanz <maroua.bouanz@gmail.com>
2023-08-22 09:49:16 +0100

    Mise en page de l'application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <h1>Recette</h1>;
-}
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
+import styles from "./App.module.scss";
 
+function App() {
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <div className="b1">Header</div>;
+}
+
+export default Header;
