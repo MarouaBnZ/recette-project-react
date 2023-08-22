135fca852cb1b4bf4bbb506057d594de61feeee0
Maroua Bouanz <maroua.bouanz@gmail.com>
2023-08-22 20:35:00 +0100

    Mise en place du contenu

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,20 @@
-import React from "react";
-
+import styles from "./Content.module.scss";
 function Content() {
-  return <div className="flex-fill b3">Content</div>;
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Decouvrez nos nouvelles recettes</h1>
+      <div className={`card p-20 ${styles.contentCard}`}>
+        <div className={styles.grid}>
+          <div className={styles.element}>element</div>
+          <div className={styles.element}>element</div>
+          <div className={styles.element}>element</div>
+          <div className={styles.element}>element</div>
+          <div className={styles.element}>element</div>
+          <div className={styles.element}>element</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
